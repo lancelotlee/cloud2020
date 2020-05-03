38ad266679aebe6a82b3bb5bd213bd3db96653ed
lancelotlee <sorlinlee93@gmail.com>
2020-05-03 11:53:13 +0800

    集成SpringCloud bus实现全局配置刷新

---
+++ cloud-config-client-3355/src/main/java/com/sorlin/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,15 @@
+package com.sorlin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 李松岭
+ * @date 2020-05-02 11:02
+ **/
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/sorlin/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.sorlin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author 李松岭
+ * @date 2020-05-02 16:47
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
