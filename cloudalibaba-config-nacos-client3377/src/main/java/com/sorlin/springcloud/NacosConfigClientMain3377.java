ef19ccf97dc1335c1bcbfced4468d74ed117e36e
lancelotlee <sorlinlee93@gmail.com>
2020-05-04 22:41:10 +0800

    Nacos作为服务配置中心演示

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/sorlin/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.sorlin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author 李松岭
+ * @date 2020-05-04 20:37
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377
+{
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
