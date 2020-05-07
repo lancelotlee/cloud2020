c52db0d57ac7c11bc64b1ecc9e1cccafb99cf910
lancelotlee <sorlinlee93@gmail.com>
2020-05-07 14:14:36 +0800

    Sentinel 熔断限流测试

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/sorlin/springcloud/MainApp8401.java
@@ -0,0 +1,18 @@
+package com.sorlin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author 李松岭
+ * @date 2020-05-06 10:15
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MainApp8401
+{
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/sorlin/springcloud/myhandler/CustomerBlockHandler.java
@@ -0,0 +1,16 @@
+package com.sorlin.springcloud.myhandler;
+
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.sorlin.springcloud.entities.CommonResult;
+
+/**
+ * @author 李松岭
+ * @date 2020-05-07 11:29
+ **/
+public class CustomerBlockHandler {
+
+    public static CommonResult handlerException(BlockException exception) {
+        return new CommonResult(2020, "自定义限流处理信息....CustomerBlockHandler");
+
+    }
+}
