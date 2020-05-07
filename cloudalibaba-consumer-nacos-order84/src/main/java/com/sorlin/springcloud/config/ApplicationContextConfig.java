d086fc2d9e090c44db9bc683d3ae2105ce4881a5
lancelotlee <sorlinlee93@gmail.com>
2020-05-07 16:14:00 +0800

    Sentinel 整合Ribbon,测试@SentinelResource

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/sorlin/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,19 @@
+package com.sorlin.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author 李松岭
+ * @date 2020-05-07 14:40
+ **/
+@Configuration
+public class ApplicationContextConfig {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
---
+++ cloudalibaba-provider-payment9004/src/main/java/com/sorlin/springcloud/PaymentMain9004.java
@@ -0,0 +1,20 @@
+package com.sorlin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author 李松岭
+ * @date 2020-05-07 14:26
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9004
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9004.class, args);
+    }
+}
+
+
